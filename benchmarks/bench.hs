787b1b2d271d1e773e865a4cc3a1f2a646bee6ec
Luke Taylor <tekul.hs@gmail.com>
2015-02-22 00:17:47 +0000

    Update benchmark code to work with latest API

--- benchmarks/bench.hs
+++ benchmarks/bench.hs
@@ -5,18 +5,25 @@ import Criterion.Main
 import Crypto.Random
 import Jose.Jws
 import Jose.Jwa
+import Jose.Jwt
 import Keys
 
 main = do
     rng <- cprgCreate `fmap` createEntropyPool :: IO SystemRNG
     let msg = "The best laid schemes o' mice and men"
+        rsaE a m  = case fst $ rsaEncode rng a jwsRsaPrivateKey m of
+            Left  _       -> error "RSA encode shouldn't fail"
+            Right (Jwt j) -> j
+        hmacE a m = case hmacEncode a jwsHmacKey m of
+            Left _        -> error "HMAC shouldn't fail"
+            Right (Jwt j) -> j
 
     defaultMain
       [ bgroup "JWS"
-          [ bench "encode RSA256" $ nf (rsaEncode RS256 jwsRsaPrivateKey) msg
-          , bench "encode RSA384" $ nf (rsaEncode RS384 jwsRsaPrivateKey) msg
-          , bench "encode RSA512" $ nf (rsaEncode RS384 jwsRsaPrivateKey) msg
-          , bench "encode HS256"  $ nf (hmacEncode HS256 jwsHmacKey) msg
-          , bench "encode HS512"  $ nf (hmacEncode HS512 jwsHmacKey) msg
+          [ bench "encode RSA256" $ nf (rsaE RS256)  msg
+          , bench "encode RSA384" $ nf (rsaE RS384)  msg
+          , bench "encode RSA512" $ nf (rsaE RS384)  msg
+          , bench "encode HS256"  $ nf (hmacE HS256) msg
+          , bench "encode HS512"  $ nf (hmacE HS512) msg
           ]
       ]
