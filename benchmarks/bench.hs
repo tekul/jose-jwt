1ce73b62f7b06534d727947943c0e66b64364e02
Mikael Bung <mkbung@gmail.com>
2016-02-16 21:20:01 +0100

    Fix criterion benchmark
    Benchmark code did not compile due to it being written towards an
    old api, plus missing dependency on the rng. Fixed benchmark, including
    adding HS384 benchmark and correcting RS512 benchmark.
    
    The RS384 benchmark is prone to failing due to a cryptonite bug
    fixed in cryptonite-0.13, use CPP to only benchmark RS384 if
    cryptonite-0.13 or above is used.

--- benchmarks/bench.hs
+++ benchmarks/bench.hs
@@ -1,17 +1,21 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, CPP #-}
 module Main where
 
 import Criterion.Main
 import Crypto.Random
+import Data.Word (Word64)
 import Jose.Jws
 import Jose.Jwa
 import Jose.Jwt
 import Keys
 
+benchRNG = drgNewTest (w, w, w, w, w) where w = 1 :: Word64
+
+fstWithRNG = fst . withDRG benchRNG
+
 main = do
-    rng <- cprgCreate `fmap` createEntropyPool :: IO SystemRNG
     let msg = "The best laid schemes o' mice and men"
-        rsaE a m  = case fst $ rsaEncode rng a jwsRsaPrivateKey m of
+        rsaE a m  = case fstWithRNG (rsaEncode a jwsRsaPrivateKey m) of
             Left  _       -> error "RSA encode shouldn't fail"
             Right (Jwt j) -> j
         hmacE a m = case hmacEncode a jwsHmacKey m of
@@ -21,9 +25,12 @@ main = do
     defaultMain
       [ bgroup "JWS"
           [ bench "encode RSA256" $ nf (rsaE RS256)  msg
+#if MIN_VERSION_cryptonite(0,13,0)
           , bench "encode RSA384" $ nf (rsaE RS384)  msg
-          , bench "encode RSA512" $ nf (rsaE RS384)  msg
+#endif
+          , bench "encode RSA512" $ nf (rsaE RS512)  msg
           , bench "encode HS256"  $ nf (hmacE HS256) msg
+          , bench "encode HS384"  $ nf (hmacE HS384) msg
           , bench "encode HS512"  $ nf (hmacE HS512) msg
           ]
       ]
