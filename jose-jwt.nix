61ed97e84c1b65340ce14c86f1d66b614235c9b1
Rebecca Turner <rbt@sent.as>
2023-12-07 09:41:42 -0500

    Use crypton instead of cryptonite (#44)
    Closes #41

--- jose-jwt.nix
+++ jose-jwt.nix
@@ -1,22 +1,22 @@
 { mkDerivation, aeson, attoparsec, base, bytestring, cereal
-, containers, criterion, cryptonite, doctest, hspec, HUnit, memory
+, containers, criterion, crypton, doctest, hspec, HUnit, memory
 , mtl, QuickCheck, stdenv, text, time, transformers
 , transformers-compat, unordered-containers, vector
 }:
 mkDerivation {
   pname = "jose-jwt";
-  version = "0.8.0";
+  version = "0.9.6";
   src = ./.;
   libraryHaskellDepends = [
-    aeson attoparsec base bytestring cereal containers cryptonite
+    aeson attoparsec base bytestring cereal containers crypton
     memory mtl text time transformers transformers-compat
     unordered-containers vector
   ];
   testHaskellDepends = [
-    aeson base bytestring cryptonite doctest hspec HUnit memory mtl
+    aeson base bytestring crypton doctest hspec HUnit memory mtl
     QuickCheck text unordered-containers vector
   ];
-  benchmarkHaskellDepends = [ base bytestring criterion cryptonite ];
+  benchmarkHaskellDepends = [ base bytestring criterion crypton ];
   homepage = "http://github.com/tekul/jose-jwt";
   description = "JSON Object Signing and Encryption Library";
   license = stdenv.lib.licenses.bsd3;
